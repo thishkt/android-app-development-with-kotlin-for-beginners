3931112d258a8d8ce475e17e48d209329e1a2c05
HKT <thishkt@gmail.com>
2023-10-07 16:00:02 +0800

    啟用 View Binding

--- MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package tw.com.hkt.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tw.com.hkt.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
