12b5a6a3ae884c7e6b93b79f7fc2b1a8af12a236
HKT <thishkt@gmail.com>
2023-10-07 15:53:32 +0800

    TextView 文字元件使用 Kotlin 程式碼設定屬性

--- MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package tw.com.hkt.myapplication
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tw.com.hkt.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myTextView.text ="HKT線上教室"
+        binding.myTextView.textSize = 50f
+        binding.myTextView.setTextColor(Color.parseColor("#2828ff"))
+        binding.myTextView.setTypeface(null, Typeface.BOLD_ITALIC)
     }
 }
