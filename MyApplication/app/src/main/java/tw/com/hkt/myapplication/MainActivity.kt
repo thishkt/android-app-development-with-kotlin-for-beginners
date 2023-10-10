ed241b821e4c1417b533a3b2d670f102687212c1
HKT <thishkt@gmail.com>
2023-10-10 11:16:31 +0800

    Intent 從Ａ頁跳到Ｂ頁，並傳遞資料

--- MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package tw.com.hkt.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import tw.com.hkt.myapplication.databinding.ActivityMainBinding
@@ -10,9 +11,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnNext.setOnClickListener{
+            val inputData = binding.etInputData.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("data_key", inputData)
+            startActivity(intent)
+        }
     }
 }
---
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/SecondActivity.kt
@@ -0,0 +1,23 @@
+package tw.com.hkt.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tw.com.hkt.myapplication.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputData = intent.getStringExtra("data_key")
+        binding.tvShowData.text = inputData
+
+        binding.btnBack.setOnClickListener{
+            finish()
+        }
+    }
+}
