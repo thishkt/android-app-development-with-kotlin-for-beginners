d8f0f1d79aeccebec163c189569ff1fc8589e867
HKT <thishkt@gmail.com>
2023-11-08 14:47:30 +0800

    Activity 和 Fragment CallBack 使用技巧

--- MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/MainActivity.kt
@@ -2,17 +2,24 @@ package tw.com.hkt.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
 import tw.com.hkt.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(),
+    DemoFragment.DemoListener {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
+
+    override fun onButtonClick(text: String) {
+        Log.d("QQQ", "onButtonClick $text")
+        binding.tvResult.text = text
+    }
+
 }
