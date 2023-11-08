2bc7fb552f13480fe0d363df712ff65d8f4d6ed6
HKT <thishkt@gmail.com>
2023-11-08 14:40:05 +0800

    Fragment ViewBinding 使用技巧

--- MyApplication/app/src/main/java/tw/com/hkt/myapplication/DemoFragment.kt
+++ MyApplication/app/src/main/java/tw/com/hkt/myapplication/DemoFragment.kt
@@ -1,59 +1,35 @@
 package tw.com.hkt.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import tw.com.hkt.myapplication.databinding.FragmentDemoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DemoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DemoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDemoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo, container, false)
+        binding = FragmentDemoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DemoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DemoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button1.setOnClickListener { buttonClicked("按鈕一") }
+        binding.button2.setOnClickListener { buttonClicked("按鈕二") }
+        binding.button3.setOnClickListener { buttonClicked("按鈕三") }
+    }
+
+    private fun buttonClicked(data: String) {
+        Log.d("QQQ", "你點擊了： $data")
     }
 }
